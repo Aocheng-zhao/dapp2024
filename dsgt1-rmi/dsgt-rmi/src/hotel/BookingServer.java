0da5981cdafead8a272ae989ad71bf6592f6b97c
aochengzhao <aocheng.zhao@student.kuleuven.be>
2024-03-31 01:23:50 +0100

    jar

--- dsgt1-rmi/dsgt-rmi/src/hotel/BookingServer.java
+++ dsgt1-rmi/dsgt-rmi/src/hotel/BookingServer.java
@@ -9,7 +9,7 @@ public class BookingServer {
         //create
         try{
             BookingManager bm = new BookingManager();
-            IBookingManager stub = (IBookingManager) UnicastRemoteObject.exportObject(bm,49260);
+            IBookingManager stub = (IBookingManager) UnicastRemoteObject.exportObject(bm,0);
             //Bind the remote object's stub in the registry
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("BookingManager",stub);
